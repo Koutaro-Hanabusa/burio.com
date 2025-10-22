71b2249dc201d39fd04411f445b85e14c86c2f1e
buri1126 <koutarouhanabusa@gmail.com>
2025-10-22 20:40:16 +0900

    好きなものに詳細を追加する

--- apps/web/src/constants/favorites.ts
+++ apps/web/src/constants/favorites.ts
@@ -4,9 +4,13 @@ export const FAVORITES: FavoriteCategory[] = [
 	{
 		category: "Tottenham Hotspur FC",
 		items: ["Son Heung-Min", "Harry Kane"],
+		detail:
+			"ロンドンにあるTottenham Hotspur FCというチームのサポーターをしています。\n気づけばもう8年目になっていました。\n部屋は選手のユニフォームで埋め尽くされていてもう空きがないです！",
 	},
 	{
 		category: "食べ物",
 		items: ["タコス", "ラーメン", "ハンバーガー"],
+		detail:
+			"特にタコスが大好きです。最近は毎週タコス食べているくらい。\n令和のタコスブームに乗り遅れるな！とこの前ネットニュースにも書いてあったのでみんなタコスを食べて火曜日を感じましょう🌮",
 	},
 ];
